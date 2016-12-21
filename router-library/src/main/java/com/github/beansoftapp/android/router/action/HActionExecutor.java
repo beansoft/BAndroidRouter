cab7f75d7d3906b3be94fe6f6a64d9fddff60fdf
LiuChangjiong <liuchangjiong@gunlei.com>
2016-12-21 21:42:38 +0800

    更新注释

--- router-library/src/main/java/com/github/beansoftapp/android/router/action/HActionExecutor.java
+++ router-library/src/main/java/com/github/beansoftapp/android/router/action/HActionExecutor.java
@@ -3,30 +3,42 @@ package com.github.beansoftapp.android.router.action;
 import java.lang.reflect.Method;
 import java.util.Map;
 
+/**
+ * Action执行类. 简化Action接口本身的行为. 注意不支持混淆.
+ * Created by beansoft on 16/12/21.
+ */
 public class HActionExecutor {
-    // 禁止子类重载此行为
-    public static final Object handleParams(HAction action, Map<String, String> map, HCallback callback) {
-        if(action == null) {return null;}
-        if (map != null && map.size() > 0) {// Map 不为空
+
+    /**
+     * 处理参数并执行回调, 如果callback为空, 则执行同步调用. 此类被Router所调用, 开发者不要覆盖它的行为.    禁止子类重载此行为
+     *
+     * @param params   键值对, 例如最终会转化成Http请求参数.
+     * @param callback 异步回调
+     * @return 同步执行时会返回执行结果, 异步时返回空.
+     * @see com.github.beansoftapp.android.router.HRouter#action(String)
+     * @see com.github.beansoftapp.android.router.HRouter#action(String, HCallback)
+     */
+    public static final Object handleParams(HAction action, Map<String, String> params, HCallback callback) {
+        if (params != null && params.size() > 0) {// Map 不为空, 有参数
             if (callback == null) {// 同步调用
                 try {
-                    // 检查当前类是否处理此方法 TODO 继承关系怎么处理?
+                    // 检查当前类是否处理此方法 TODO Action 继承关系怎么处理?
                     Method m = action.getClass().getDeclaredMethod("action", Object.class);
-                    if(m!=null) {
-                        return action.action(map);
+                    if (m != null) {
+                        return action.action(params);
                     }
                 } catch (NoSuchMethodException e) {
                     e.printStackTrace();
                     return action.action();
                 }
-            } else {
+            } else {// 异步调用, 有回调
                 callback.start();
 
                 try {
-                    // 检查当前类是否处理此方法 TODO 继承关系怎么处理?
+                    // 检查当前类是否处理此方法 TODO Action 继承关系怎么处理?
                     Method m = action.getClass().getDeclaredMethod("action", HCallback.class);
-                    if(m!=null) {
-                        action.action(map, callback);
+                    if (m != null) {
+                        action.action(params, callback);
                     }
                 } catch (NoSuchMethodException e) {
                     e.printStackTrace();
@@ -34,7 +46,8 @@ public class HActionExecutor {
                 }
             }
             return null;
-        } else if (callback == null) {
+        }
+        else if (callback == null) {
             return action.action();
         } else {
             callback.start();
